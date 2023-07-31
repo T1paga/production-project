c89843d621c916f6594fab8bba83a83c8c26aad9
T1paga <okru.shko@bk.ru>
2023-07-31 20:15:09 +0700

    + алиасы в jest, + скрины, + theme addon для storybook

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -16,3 +16,13 @@ const Template: ComponentStory<typeof ArticlesPage> = (args) => <ArticlesPage {.
 export const Normal = Template.bind({})
 Normal.args = {}
 Normal.decorators = [StoreDecorator({})]
+Normal.parameters = {
+	mockData: [
+		{
+			url: __API__ + '/articles?_limit=3',
+			method: 'GET',
+			status: 200,
+			response: []
+		}
+	]
+}
