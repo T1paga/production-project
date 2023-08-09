06b181c09d7ecab8b2a006631181a6dbc920c793
T1paga <okru.shko@bk.ru>
2023-08-09 18:13:40 +0700

    [Редизайн] Изменил sidebar, icon, buttons

--- src/features/LangSwitcher/ui/LangSwitcher/LangSwitcher.tsx
+++ src/features/LangSwitcher/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,7 +1,12 @@
-import { classNames } from '@/shared/lib/classNames/classNames'
 import { useTranslation } from 'react-i18next'
-import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button'
 import { memo } from 'react'
+import { classNames } from '@/shared/lib/classNames/classNames'
+import {
+	Button as ButtonDeprecated,
+	ButtonTheme
+} from '@/shared/ui/deprecated/Button'
+import { ToggleFeatures } from '@/shared/lib/features'
+import { Button } from '@/shared/ui/redesigned/Button'
 
 interface LangSwitcherProps {
 	className?: string
@@ -12,18 +17,26 @@ export const LangSwitcher = memo(({ className, short }: LangSwitcherProps) => {
 	const { t, i18n } = useTranslation()
 
 	const toggle = async () => {
-		// eslint-disable-next-line @typescript-eslint/no-floating-promises
 		i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
 	}
 
 	return (
-		<Button
-			className={classNames('', {}, [className])}
-			theme={ButtonTheme.CLEAR}
-			// eslint-disable-next-line @typescript-eslint/no-misused-promises
-			onClick={toggle}
-		>
-			{t(short ? 'Короткий язык' : 'Язык')}
-		</Button>
+		<ToggleFeatures
+			feature="isAppRedesigned"
+			on={
+				<Button variant="clear" onClick={toggle}>
+					{t(short ? 'Короткий язык' : 'Язык')}
+				</Button>
+			}
+			off={
+				<ButtonDeprecated
+					className={classNames('', {}, [className])}
+					theme={ButtonTheme.CLEAR}
+					onClick={toggle}
+				>
+					{t(short ? 'Короткий язык' : 'Язык')}
+				</ButtonDeprecated>
+			}
+		/>
 	)
 })
