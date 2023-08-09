b707f5386a7e45b40a1796e2660a8c53222ac299
T1paga <okru.shko@bk.ru>
2023-08-09 13:18:27 +0700

    инициализация authData через сервер

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,18 +3,24 @@ import { classNames } from '@/shared/lib/classNames/classNames'
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme'
 import { Navbar } from '@/widgets/Navbar'
 import { Sidebar } from '@/widgets/Sidebar'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUserInited, userActions } from '@/entities/User'
+import { useSelector } from 'react-redux'
+import { getUserInited, initAuthData } from '@/entities/User'
 import { AppRouter } from './providers/router'
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch'
+import { PageLoader } from '@/widgets/PageLoader'
 
 function App() {
 	const { theme } = useTheme()
-	const dispatch = useDispatch()
+	const dispatch = useAppDispatch()
 	const inited = useSelector(getUserInited)
 
 	useEffect(() => {
-		dispatch(userActions.initAuthData())
+		dispatch(initAuthData())
 	}, [dispatch])
+
+	if (!inited) {
+		return <PageLoader />
+	}
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
+export { initAuthData } from './model/services/initAuthData'
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData'
 export { getUserInited } from './model/selectors/getUserInited/getUserInited'
 export { userReducer, userActions } from './model/slice/userSlice'
