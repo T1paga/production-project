59fa746f464f24569a9e0bf3064fc1e0cfeb31d9
T1paga <okru.shko@bk.ru>
2023-08-11 14:14:26 +0700

    [Рефакторинг] Изменил модальное окно, шторку и портал

--- src/shared/ui/redesigned/Drawer/Drawer.tsx
+++ src/shared/ui/redesigned/Drawer/Drawer.tsx
@@ -1,10 +1,14 @@
+import React, { memo, ReactNode, useCallback, useEffect } from 'react'
 import { classNames } from '@/shared/lib/classNames/classNames'
-import { memo, type ReactNode, useCallback, useEffect } from 'react'
-import { AnimationProvider, useAnimationLibs } from '@/shared/lib/components/AnimationProvider'
-import { Overlay } from '../../redesigned/Overlay/Overlay'
+import {
+	AnimationProvider,
+	useAnimationLibs
+} from '@/shared/lib/components/AnimationProvider'
+import { Overlay } from '../Overlay/Overlay'
 import cls from './Drawer.module.scss'
-import { Portal } from '../../redesigned/Portal/Portal'
+import { Portal } from '../Portal/Portal'
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme'
+import { toggleFeatures } from '@/shared/lib/features'
 
 interface DrawerProps {
 	className?: string
@@ -20,13 +24,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
 	const { Spring, Gesture } = useAnimationLibs()
 	const [{ y }, api] = Spring.useSpring(() => ({ y: height }))
 	const { theme } = useTheme()
-	const {
-		className,
-		children,
-		onClose,
-		isOpen,
-		lazy
-	} = props
+	const { className, children, onClose, isOpen, lazy } = props
 
 	const openDrawer = useCallback(() => {
 		api.start({ y: 0, immediate: false })
@@ -68,7 +66,10 @@ export const DrawerContent = memo((props: DrawerProps) => {
 			}
 		},
 		{
-			from: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true
+			from: () => [0, y.get()],
+			filterTaps: true,
+			bounds: { top: 0 },
+			rubberband: true
 		}
 	)
 
@@ -79,12 +80,27 @@ export const DrawerContent = memo((props: DrawerProps) => {
 	const display = y.to((py) => (py < height ? 'block' : 'none'))
 
 	return (
-		<Portal>
-			<div className={classNames(cls.Drawer, {}, [className, theme, 'app_drawer'])}>
+		<Portal element={document.getElementById('app') ?? document.body}>
+			<div
+				className={classNames(cls.Drawer, {}, [
+					className,
+					theme,
+					'app_drawer',
+					toggleFeatures({
+						name: 'isAppRedesigned',
+						on: () => cls.drawerNew,
+						off: () => cls.drawerOld
+					})
+				])}
+			>
 				<Overlay onClick={close} />
 				<Spring.a.div
 					className={cls.sheet}
-					style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
+					style={{
+						display,
+						bottom: `calc(-100vh + ${height - 100}px)`,
+						y
+					}}
 					{...bind()}
 				>
 					{children}
@@ -104,9 +120,6 @@ const DrawerAsync = (props: DrawerProps) => {
 	return <DrawerContent {...props} />
 }
 
-/**
- * @deprecated
- */
 export const Drawer = (props: DrawerProps) => {
 	return (
 		<AnimationProvider>
--- src/shared/ui/redesigned/Modal/Modal.stories.tsx
+++ src/shared/ui/redesigned/Modal/Modal.stories.tsx
@@ -1,6 +1,7 @@
-import type { ComponentMeta, ComponentStory } from '@storybook/react'
-import { Modal } from '../Modal/Modal'
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Modal } from './Modal'
 import { Theme } from '@/shared/const/theme'
 
 export default {
@@ -16,12 +17,14 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />
 export const Primary = Template.bind({})
 Primary.args = {
 	isOpen: true,
-	children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n '
+	children:
+		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n '
 }
 
 export const Dark = Template.bind({})
 Dark.args = {
 	isOpen: true,
-	children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n '
+	children:
+		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n '
 }
 Dark.decorators = [ThemeDecorator(Theme.DARK)]
